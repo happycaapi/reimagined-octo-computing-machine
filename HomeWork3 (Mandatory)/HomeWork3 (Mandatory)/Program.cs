4a1f3c39ad28c60d424b08d736d5e570e1e648d3
happycaapi <cm284310@ohio.edu>
2016-10-27 17:18:12 -0500

    This is homework three solving a factorial

---
+++ HomeWork3 (Mandatory)/HomeWork3 (Mandatory)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3__Mandatory_
+{
+    class Program
+    {
+        static void Main(string[] args) //
+
+        {
+            Console.WriteLine("Please input the integer you wish to have factorialized:");
+            string thingy = Console.ReadLine();
+            int pseudothingy = int.Parse(thingy);
+            int superthingy = 1;
+            while (pseudothingy > 1)
+            { superthingy = (superthingy*pseudothingy);
+              pseudothingy = pseudothingy - 1;
+            }
+            Console.WriteLine("your answer is {0}", superthingy);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
