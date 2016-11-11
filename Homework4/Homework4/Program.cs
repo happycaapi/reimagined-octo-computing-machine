e92af6641f6fdebfff4ef0ecfafe25aae0be527e
happycaapi <cm284310@ohio.edu>
2016-11-10 19:27:21 -0600

    Homework4

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the integer of the number of factorials you wish to display:");
+            string thingy = Console.ReadLine();
+            int pseudothingy = int.Parse(thingy);
+            int x;
+            int y=pseudothingy;
+            int quickans=pseudothingy;
+            
+            for (y=pseudothingy;y>0;y--) 
+            { 
+
+                for (x = (pseudothingy - 1); x > 1; x--)
+                {
+                        
+                        quickans=(x * quickans);
+                    
+                }
+                Console.WriteLine("{0} = {1}", y, quickans);
+                pseudothingy = pseudothingy - 1;
+                quickans = pseudothingy;
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
